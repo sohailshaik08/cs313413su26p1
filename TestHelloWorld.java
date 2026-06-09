27eef6143a55f2e5444c3bdd0b0ecf03948ddecd
sohailshaik08 <sshaik@luc.edu>
2026-06-08 22:35:02 -0500

    Add files via upload
    Finished Project 1

---
+++ TestHelloWorld.java
@@ -0,0 +1,52 @@
+package hw;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestHelloWorld {
+
+  private HelloWorld fixture;
+
+  @Before
+  public void setUp() throws Exception {
+    fixture = new HelloWorld();
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    fixture = null;
+  }
+
+  @Test
+  public void initialization() { // this test is OK as-is, it should pass with no problem
+    assertNotNull(fixture);
+  }
+
+  @Test
+  public void getMessage() { // this test is OK as-is, it should pass with no problem
+    assertNotNull(fixture);
+    assertEquals("hello world", fixture.getMessage());
+  }
+
+  @Test
+  public void getYear() { // this test is OK, fix HelloWorld.java to make it pass!
+    assertNotNull(fixture);
+    assertEquals(2026, fixture.getYear());
+  }
+
+  @Test
+  public void getMessageInList() { // FIXED: Changed list.get(1) to list.get(0)
+    var list = Arrays.asList(fixture);
+    assertEquals("hello world", list.get(0).getMessage());
+  }
+
+  @Test
+  public void getYearInList() { // FIXED: Changed list.get(1) to list.get(0)
+    var list = Arrays.asList(fixture);
+    assertEquals(2026, list.get(0).getYear());
+  }
+}
